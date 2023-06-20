db97c0f2d80781da216d0c6f66ac9134ba49bd49
Arthur <titan_0396@mail.ru>
2023-06-20 13:23:58 +0600

    comment

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,52 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Header from '../header';
-import RandomPlanet from '../random-planet';
-import ErrorButton from '../error-button';
-import PeoplePage from '../people-page';
+import Header from "../header";
+import RandomPlanet from "../random-planet";
+import ErrorButton from "../error-button";
+import PeoplePage from "../people-page";
 import ErrorBoundry from "../error-boundry";
 
-import './app.css';
+import "./app.css";
 
 export default class App extends Component {
-
   state = {
-    showRandomPlanet: true
+    showRandomPlanet: true,
   };
 
   toggleRandomPlanet = () => {
     this.setState((state) => {
       return {
-        showRandomPlanet: !state.showRandomPlanet
-      }
+        showRandomPlanet: !state.showRandomPlanet,
+      };
     });
   };
 
   render() {
-
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet/> :
-      null;
+    const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
 
     return (
       <ErrorBoundry>
         <div className="stardb-app">
           <Header />
-          { planet }
+          {planet}
 
           <div className="row mb2 button-row">
             <button
               className="toggle-planet btn btn-warning btn-lg"
-              onClick={this.toggleRandomPlanet}>
+              onClick={this.toggleRandomPlanet}
+            >
               Toggle Random Planet
             </button>
             <ErrorButton />
           </div>
 
           <PeoplePage />
-
         </div>
       </ErrorBoundry>
+      //комментарий
     );
   }
 }
